9b7c5e449d3a111a3cdc63b9ef868ef2282cadb5
JG8203 <joshua_gilo@dlsu.edu.ph>
2023-06-25 00:02:59 +0800

    First com

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 import java.util.function.Function;
 
 public class Main {
-    private static final String[] FILE_NAMES = {"almostsorted.txt", "random100.txt", "random25000.txt", "random50000.txt",
-            "random75000.txt", "random100000.txt", "totallyreversed.txt"};
+    private static final String[] FILE_NAMES = {"10.txt", "random100.txt", "random25000.txt", "random50000.txt",
+            "random75000.txt", "random100000.txt", "totallyreversed.txt", "almostsorted.txt"};
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
@@ -22,11 +22,11 @@ public class Main {
         performAndTimeSort("Insertion Sort", i -> sortingAlgorithms.insertionSort(Arrays.copyOf(records, n), n));
         performAndTimeSort("Selection Sort", i -> sortingAlgorithms.selectionSort(Arrays.copyOf(records, n), n));
         performAndTimeSort("Merge Sort", i -> sortingAlgorithms.mergeSort(Arrays.copyOf(records, n), 0, n-1));
-        performAndTimeSort("Bogo Sort", i -> sortingAlgorithms.bogoSort(Arrays.copyOf(records, n), n));
+        // performAndTimeSort("Bogo Sort", i -> sortingAlgorithms.bogoSort(Arrays.copyOf(records, n), n));
     }
 
     private static void performAndTimeSort(String algorithmName, Function<Integer, Long> sortAction) {
-        long totalExecutionTime = 0;
+        double totalExecutionTime = 0;
         long ctr = 0;
         for (int i = 0; i < 5; i++) {
             long startTime = System.currentTimeMillis();
@@ -35,7 +35,8 @@ public class Main {
             totalExecutionTime += executionTime;
             System.out.println((i + 1) + " " + algorithmName + " took " + executionTime + " ms");
         }
-        System.out.println(algorithmName + " Average Execution time: " + totalExecutionTime / 5 + " ms");
+        double averageExecutionTime = totalExecutionTime/5;
+        System.out.println(algorithmName + " Average Execution time: " + averageExecutionTime / 5 + " ms");
         System.out.println(algorithmName + " Total Operations: " + ctr);
         System.out.println();
     }

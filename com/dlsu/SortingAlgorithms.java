2ce2496e04a496c544b411ca74df1b7d6702ecf7
JG8203 <joshua_gilo@dlsu.edu.ph>
2023-07-08 14:40:13 +0800

    changed writer.

--- com/dlsu/SortingAlgorithms.java
+++ com/dlsu/SortingAlgorithms.java
@@ -5,78 +5,100 @@ import java.io.PrintWriter;
 
 public class SortingAlgorithms {
 
-    private static void swap(Record[] array, int firstIndex, int secondIndex) {
+    private static void swap(Record[] array, int firstIndex, int secondIndex, PrintWriter writer) {
         Record temp = array[firstIndex];
         array[firstIndex] = array[secondIndex];
         array[secondIndex] = temp;
+        printArray(array, writer);
     }
+
     public long insertionSort(Record[] array, int length, PrintWriter writer) {
-        long ctr = 1; // for the outer for loop initialization
+        long ctr = 1;
         for (int i = 1; i < length; ++i) {
-            Record key = array[i]; ctr++; // for the key assignment
+            Record key = array[i]; ctr++;
             int j = i - 1; ctr++;
-            ctr++; // for the j initialization
+            ctr++;
             while (j >= 0 && array[j].getIdNumber() > key.getIdNumber()) {
-                ctr++; // for the while loop condition
+                ctr++;
                 array[j + 1] = array[j]; ctr++;
                 j = j - 1; ctr++;
-                ctr++; // for the while loop condition
+                ctr++;
             }
             array[j + 1] = key; ctr++;
-            writer.println("Iteration " + i + ":");
-            for (Record r : array) {
-                writer.println(r.getIdNumber() + " " + r.getName());
-            }
-            writer.println();
+            printArray(array, writer);
         }
         return ctr;
     }
 
-    // Similar modifications for other sorting methods...
-
-
     public long selectionSort(Record[] array, int length, PrintWriter writer) {
-        long ctr = 1; // for the outer for loop initialization
+        long ctr = 1;
         for (int i = 0; i < length - 1; i++) {
-            int minIndex = i;
-            ctr++; // for the minIndex initialization
-            ctr++; // for the inner for loop initialization
+            int minIndex = i; ctr++;
+            ctr++;
             for (int j = i + 1; j < length; j++) {
-                ctr++; // for the inner for loop condition
+                ctr++;
                 if (array[j].getIdNumber() < array[minIndex].getIdNumber()) {
                     minIndex = j;
                 }
             }
-            swap(array, minIndex, i);
-            ctr++; // for the swap operation
-            writer.println("Iteration " + (i + 1) + ":");
-            for (Record r : array) {
-                writer.println(r.getIdNumber() + " " + r.getName());
-            }
-            writer.println();
+            swap(array, minIndex, i, writer); ctr++;
         }
         return ctr;
     }
 
     public long mergeSort(Record[] array, int start, int end, PrintWriter writer) {
-        long ctr = 1; // for the outer if condition
+        long ctr = 1;
         if (start < end) {
-            ctr++; // for the mid assignment
+            ctr++;
             int mid = (start + end) / 2;
-            ctr++; // for the first recursive call
             ctr += mergeSort(array, start, mid, writer);
-            ctr++; // for the second recursive call
             ctr += mergeSort(array, mid + 1, end, writer);
-
-            // ... Rest of mergeSort code ...
-
-            writer.println("After merge, Iteration " + ctr + ":");
-            for (Record r : array) {
-                writer.println(r.getIdNumber() + " " + r.getName());
-            }
-            writer.println();
+            ctr += merge(array, start, mid, end, writer);
         }
         return ctr;
+    }
+
+    private long merge(Record[] array, int start, int mid, int end, PrintWriter writer) {
+        int n1 = mid - start + 1;
+        int n2 = end - mid;
+
+        Record[] L = new Record[n1];
+        Record[] R = new Record[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = array[start + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = array[mid + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = start;
+        while (i < n1 && j < n2) {
+            if (L[i].getIdNumber() <= R[j].getIdNumber()) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = R[j];
+            j++;
+            k++;
+        }
+
+        printArray(array, writer); // print array after merging
+
+        return k; // return number of operations
     }
 
     public static long bogoSort(Record[] array, int length, PrintWriter writer) {
@@ -91,12 +113,7 @@ public class SortingAlgorithms {
                 array[i] = array[swapPosition];
                 array[swapPosition] = temp;
             }
-
-            writer.println("Iteration " + ctr + ":");
-            for (Record r : array) {
-                writer.println(r.getIdNumber() + " " + r.getName());
-            }
-            writer.println();
+            printArray(array, writer);
         }
         return ctr;
     }
@@ -108,5 +125,12 @@ public class SortingAlgorithms {
             }
         }
         return true;
+    }
+
+    private static void printArray(Record[] array, PrintWriter writer) {
+        for (Record r : array) {
+            writer.print(r.getIdNumber() + " ");
+        }
+        writer.println();
     }
 }
